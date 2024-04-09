7f57af1e71fb0eda0c384221d241b045a2911bf6
Artem <hitko21@gmail.com>
2024-04-09 13:24:55 +0300

    new day, new activity

---
+++ lesson_02.py
@@ -0,0 +1,22 @@
+# Задача 1
+# Запитайте в користувача два числа та поверніть йому суму цих чисел.
+# a = int(input("Enter number 1: "))
+# b = int(input("Enter number 2: "))
+# print(a + b)
+
+
+# Задача 2
+# Запитайте в користувача сторони прямокутника і за допомогою математичної формули та пайтона виведіть йому периметр та площу цього прямокутника.
+# a = int(input("Enter number 1: "))
+# b = int(input("Enter number 2: "))
+# print(a + a + b + b)
+# print(a * a + b * b)
+# Задача 3 (з уроку)
+# Зробіть задачу з уроку але що б знижка була у відсотках.
+
+price = float(input())
+discount = float(input())
+reguraly_price = price
+print(reguraly_price)
+discout_size = price * (discount / 100)
+print(price - discout_size)
